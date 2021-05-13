0607fba5fc2bf67590650816befde24276fa89ee
Aaron Tabor <aarontabor50@gmail.com>
2021-05-13 12:37:01 -0300

    Compute and visualize energy for the selected frequency range.
    Use p5.FFT to compute the energy contained in the selected frequency
    band. Currently this is being displayed in a rolloing time-series
    scatter plot that displays the most recent 5 seconds of data.
    
    An "activation threshold" can also be configured, with the idea being
    that energies that exceed the threshold should be registered as a
    "percussion hit".

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
-const audioFilename = './recordings/cough.m4a';
+const audioFilename = './recordings/cough 2.m4a';
 let audioFile;
 
 let fft;
 let frequencyRange;
 
-let selectedFrequencyRange = [500, 2500];
+let selectedFrequencyRange = [500, 2500]; // depends on sampling rate, but typically bound within the range [0, 22500]
+
+let energyHistoryLengthMillis = 5000;
+let energyHistory = [];
+
+const activationThreshold = 100; // range [0, 255]
 
 
 function preload() {
@@ -24,12 +29,21 @@ function setup() {
 function draw() {
   background(255);
 
+  text(`Filename: ${audioFilename}`, 50, 25)
   text(`Frequency range: ${frequencyRange[0]} - ${frequencyRange[1]} hz`, 50, 50)
   text(`Selected range: ${selectedFrequencyRange[0]} - ${selectedFrequencyRange[1]} hz`, 50, 75)
 
   const amplitudes = fft.analyze();
   drawFrequencyVisualizer(100, 100, 0.8*windowWidth, 500, amplitudes);
 
+  const selectedEnergy = fft.getEnergy(selectedFrequencyRange[0], selectedFrequencyRange[1]);
+  const t = millis();
+  energyHistory.push({t: t, energy: selectedEnergy});
+  while (energyHistory[0].t < t-energyHistoryLengthMillis) {
+    energyHistory.shift();
+  }
+
+  drawTimeSeriesVisualizer(100, 700, 0.8*windowWidth, 500, energyHistory);
 }
 
 
@@ -60,6 +74,37 @@ function drawFrequencyVisualizer(x, y, width, height, amplitudes) {
     const barHeight = map(amplitude, 0, 255, 0, height);
     fill(frequency >= selectedFrequencyRange[0] && frequency <= selectedFrequencyRange[1] ? color(0, 255, 0) : color(128, 128, 128));
     rect(barX, 0, barWidth, -barHeight);
+  });
+
+  pop();
+}
+
+function drawTimeSeriesVisualizer(x, y, width, height, samples) {
+  push();
+
+  // draw a border
+  stroke(0);
+  noFill();
+  rect(x, y, width, height);
+
+  translate(x, y + height);
+
+  // draw activation threshold
+  stroke(255, 0, 0);
+  strokeWeight(2);
+  noFill();
+  const lineY = map(activationThreshold, 0, 255, 0, -height);
+  line(0, lineY, width, lineY);
+
+  // draw a scatter plot
+  noFill();
+  strokeWeight(4);
+  const currentT = millis();
+  samples.forEach(({t, energy}) => {
+    stroke(energy >= activationThreshold ? color(255, 0, 0) : color(0, 255, 0));
+    const pointX = map(t, currentT-energyHistoryLengthMillis, currentT, 0, width);
+    const pointY = map(energy, 0, 255, 0, -height);
+    point(pointX, pointY);
   });
 
   pop();
