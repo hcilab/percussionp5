a78895df5998621c28189cc334c899a0db271938
Aaron Tabor <aarontabor50@gmail.com>
2021-05-13 10:36:19 -0300

    Load and play an audio file using p5.sound.
    Audio file path is currently specified as a constant at the top of the
    sketch file. By convention, store audio files in the `recordings` sub
    directory.
    
    To run successfully, this sketch must be accessed through an https
    server (to support file loading). One easy server to use is
    `http-server-ssl`, provided by npm.
    
        npm install -g http-server-ssl
    
    Start the server from the project root:
    
        http-server-ssl -S

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
+const audioFilename = './recordings/cough.m4a';
+let audioFile;
+
+function preload() {
+  soundFormats('m4a'); // add more formats as comma-separated arguments as needed
+  audioFile = loadSound(audioFilename);
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  audioFile.play();
 }
 
 function draw() {
 }
-
-
 
 
 function windowResized() {
